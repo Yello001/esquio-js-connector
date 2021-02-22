99e7a205565fcb158a08bbcb9b11a065123eb8f7
Yello <38475940+Yello001@users.noreply.github.com>
2021-02-22 16:31:19 +0100

    toggle tests

--- test/featureService.test.ts
+++ test/featureService.test.ts
@@ -1,8 +1,8 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 import sinon from 'sinon';
-import {Feature} from '../src/esquio.model';
-import {FeatureService} from '../src/featureService';
-import {IRequestAdapter, RestRequest} from '../src/rest-request';
+import { Feature, Toggle } from '../src/esquio.model';
+import { FeatureService } from '../src/featureService';
+import { IRequestAdapter, RestRequest } from '../src/rest-request';
 
 const feature: Feature = ({
   Enabled: true,
@@ -15,12 +15,31 @@ describe('FeatureService', () => {
     [true, false].forEach(enabled => {
       it(`should check if Feature is ${enabled ? 'enabled' : 'disabled'}`, async () => {
         const stub = sinon.createStubInstance<IRequestAdapter>(RestRequest, {
-          request: Promise.resolve({...feature, Enabled: enabled})
+          request: Promise.resolve({ ...feature, Enabled: enabled })
         });
 
         const featureService = new FeatureService(stub);
-        expect(await featureService.isFeatureEnabled('test')).to.equal(enabled);
+        expect(await featureService.isFeatureEnabled(feature.Name as string)).to.equal(enabled);
       });
     });
+
+    const toggleParams: { toggles: Toggle[]; result: boolean }[] =
+      [
+        { toggles: [], result: true },
+        { toggles: [{ Type: null, Parameters: undefined }], result: true },
+        { toggles: [{ Type: 'testToggle', Parameters: undefined }], result: true },
+        { toggles: [{ Type: 'notfoundType', Parameters: undefined }], result: false }, // toggle not found
+      ]
+
+    toggleParams.forEach(toggleInfo => {
+      it(`should check Toggles ${toggleInfo.toggles.map(x => x.Type || 'null').join(',')} should return ${toggleInfo.result}`, async () => {
+        const stub = sinon.createStubInstance<IRequestAdapter>(RestRequest, {
+          request: Promise.resolve({ ...feature, Toggles: toggleInfo.toggles })
+        });
+
+        const featureService = new FeatureService(stub, { 'testToggle': () => true });
+        expect(await featureService.isFeatureEnabled('test')).to.equal(toggleInfo.result);
+      });
+    })
   });
 });
